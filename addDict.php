7187a22c062d934219b5181dad3f66c3d701839d
tanszhe <1018595261@qq.com>
2017-12-27 21:27:33 +0800

    demo

--- addDict.php
+++ addDict.php
@@ -9,7 +9,9 @@
 //定义词典文件路径
 define('_VIC_WORD_DICT_PATH_',__DIR__.'/Data/dict.igb');
 
-require __DIR__.'/Lib/VicDict.php';
+require __DIR__.'/vendor/autoload.php';
+
+use Lizhichao\Word\VicDict;
 
 //目前可支持 igb 和 json 两种词典库格式；igb需要安装igbinary扩展，igb文件小，加载快
 $dict = new VicDict('igb');
