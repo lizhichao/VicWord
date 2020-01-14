e2ce9f175eaafb016200cdd8a79064c151adda9e
Km.Van <kmvan.com@gmail.com>
2020-01-14 22:49:56 +0800

    Fix missing dict path
    Fix missing dict path
    Adjust function slash for better performance

--- Lib/VicDict.php
+++ Lib/VicDict.php
@@ -11,10 +11,7 @@ namespace Lizhichao\Word;
 class VicDict
 {
     private $word = [];
-    /**
-     * 词典地址
-     * @var string
-     */
+
     private $code = 'utf-8';
 
     private $end = ['\\' => 1];
@@ -24,25 +21,27 @@ class VicDict
     private $end_key = '\\';
 
     private $type = 'igb';
+    
+    private $dictPath = '';
 
     public function __construct($type = 'igb')
     {
         $this->type = $type;
         
         // set default dict path
-        $dictPath = \defined('\\_VIC_WORD_DICT_PATH_') ? \_VIC_WORD_DICT_PATH_ : \dirname(__DIR__) . "/Data/dict.{$type}";
+        $this->dictPath = \defined('\\_VIC_WORD_DICT_PATH_') ? \_VIC_WORD_DICT_PATH_ : \dirname(__DIR__) . "/Data/dict.{$type}";
         
-        if( ! \file_exists($dictPath)) {
+        if ( ! \file_exists($dictPath)) {
             throw new \Exception("Invalid dict file: {$dictPath}");
         }
 
         // check dict type
         switch ($this->type) {
             case 'igb':
-                $this->word = \igbinary_unserialize(\file_get_contents($dictPath));
+                $this->word = \igbinary_unserialize(\file_get_contents($this->dictPath));
                 break;
             case 'json':
-                $this->word = \json_decode(\file_get_contents($dictPath), true);
+                $this->word = \json_decode(\file_get_contents($this->dictPath), true);
                 break;
             default:
                 throw new \Exception("Invalid dict type.");
@@ -58,9 +57,11 @@ class VicDict
     {
         $this->end = ['\\x' => $x] + $this->default_end;
         $word = $this->filter($word);
+        
         if ($word) {
             return $this->merge($word);
         }
+        
         return false;
     }
 
@@ -69,37 +70,42 @@ class VicDict
         $ar = $this->toArr($word);
         $br = $ar;
         $wr = &$this->word;
+        
         foreach ($ar as $i => $v) {
-            array_shift($br);
+            \array_shift($br);
             if (!isset($wr[$v])) {
                 $wr[$v] = $this->dict($br, $this->end);
+                
                 return true;
             } else {
                 $wr = &$wr[$v];
             }
         }
+        
         if (!isset($wr[$this->end_key])) {
             foreach ($this->end as $k => $v) {
                 $wr[$k] = $v;
                 $wr[$k] = $v;
             }
         }
+        
         return true;
     }
 
     public function save()
     {
-        if ($this->type == 'igb') {
-            $str = igbinary_serialize($this->word);
+        if ($this->type === 'igb') {
+            $str = \igbinary_serialize($this->word);
         } else {
-            $str = json_encode($this->word);
+            $str = \json_encode($this->word);
         }
-        return file_put_contents(_VIC_WORD_DICT_PATH_, $str);
+        
+        return \file_put_contents($this->dictPath, $str);
     }
 
     private function filter($word)
     {
-        return str_replace(["\n", "\t"], '', trim($word));
+        return \str_replace(["\n", "\t"], '', \trim($word));
     }
 
 
@@ -114,11 +120,13 @@ class VicDict
 
     private function toArr($str)
     {
-        $l = mb_strlen($str, $this->code);
+        $l = \mb_strlen($str, $this->code);
         $r = [];
+        
         for ($i = 0; $i < $l; $i++) {
-            $r[] = mb_substr($str, $i, 1, $this->code);
+            $r[] = \mb_substr($str, $i, 1, $this->code);
         }
+        
         return $r;
     }
 
